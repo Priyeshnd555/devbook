b37a7a2d6e84484bf20a3df35e9416d4fc9dffa0
Priyesh ND <priyesh.nd@logixal.com>
2025-12-13 11:36:27 +0530

    TS:TYPE:FIX:minor

--- app/components/HeaderActions.tsx
+++ app/components/HeaderActions.tsx
@@ -49,7 +49,7 @@ const HeaderActions: React.FC<HeaderActionsProps> = ({
           initial={{ opacity: 0, scale: 0.95, y: -10 }}
           animate={{ opacity: 1, scale: 1, y: 0 }}
           exit={{ opacity: 0, scale: 0.95, y: -10 }}
-          transition={{ duration: 0.1 }}
+          transition={true}
           className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
         >
           <div className="px-1 py-1">
